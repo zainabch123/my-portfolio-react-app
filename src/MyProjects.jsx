cf375d1a951846e841cfb44e5de47753cf8242ce
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-07-12 11:35:35 +0100

    Updated project section

--- src/MyProjects.jsx
+++ src/MyProjects.jsx
@@ -1,15 +1,38 @@
 export default function MyProjects() {
-    return (
-      <section className="projects-section">
-        <h2>My Projects</h2>
-        <p>Check out some of the projects I'm working on.</p>
-        <div className="project-links">
-          <div className="project1">Project 1</div>
-          <div className="project2">Project 2</div>
-          <div className="project3">Project 3</div>
-          <div className="project4">Project 4</div>
-          <div className="project5">Project 5</div>
+  return (
+    <section className="projects-section">
+      <h2>My Projects</h2>
+      <p>Check out some of the projects I'm working on.</p>
+      <section className="project-links">
+        <div className="project-container">
+          <div className="project-image">Image</div>
+          <div className="project-text">
+            <h3>Project Name</h3>
+            <p>Project description</p>
+          </div>
+        </div>
+        <div className="project-container">
+          <div className="project-image">Image</div>
+          <div className="project-text">
+            <h3>Project Name</h3>
+            <p>Project description</p>
+          </div>
+        </div>
+        <div className="project-container">
+          <div className="project-image">Image</div>
+          <div className="project-text">
+            <h3>Project Name</h3>
+            <p>Project description</p>
+          </div>
+        </div>
+        <div className="project-container">
+          <div className="project-image">Image</div>
+          <div className="project-text">
+            <h3>Project Name</h3>
+            <p>Project description</p>
+          </div>
         </div>
       </section>
-    );
+    </section>
+  );
 }
